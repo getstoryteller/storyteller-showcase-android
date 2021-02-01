43d3e2d777ddbeb20017ccb4013f473fcbda0fa1
jxie93 <zhihao420@hotmail.com>
2021-02-01 15:56:48 +0000

    Add docs and links in comments

--- app/src/main/java/com/example/storytellerSampleAndroid/MainActivity.kt
+++ app/src/main/java/com/example/storytellerSampleAndroid/MainActivity.kt
@@ -24,25 +24,49 @@ class MainActivity : AppCompatActivity(), StorytellerRowViewDelegate {
         //enable sdk logging for debug
         //Storyteller.enableLogging = true
 
-        //set callbacks
+        /*
+        The delegate is used for the SDK to manage events from a StorytellerRowView instance
+        Assign it to the appropriate activity to receive callbacks
+        For more info, see - https://docs.getstoryteller.com/documents/android-sdk/StorytellerRowViewDelegate#storytellerrowviewdelegate
+         */
         val storytellerRowView = findViewById<StorytellerRowView>(R.id.channelRowView)
         storytellerRowView.delegate = this
 
         //setup user
         val userId = UUID.randomUUID().toString()
-        //initialize sdk
+
+        /*
+        The SDK requires initialization before it can be used
+        This can be done by using a valid API key
+        For more info, see - https://docs.getstoryteller.com/documents/android-sdk/GettingStarted#sdk-initialization
+         */
         Storyteller.initialize("[API KEY]", {
             Log.i("Storyteller Sample", "initialize success $userId")
+
+            /*
+            Authenticate a user by setting details containing an UUID
+            For more info, see - https://docs.getstoryteller.com/documents/android-sdk/Users
+             */
             Storyteller.setUserDetails(UserInput(userId))
+
+            /*
+            Tell the SDK to load the latest data from the API
+             */
             storytellerRowView.reloadData({
                 handleDeepLink(intent?.data)
                 refreshLayout.isRefreshing = false
             })
+
+
         },{ Log.i("Storyteller Sample", "initialize failed, error $it") })
 
         //setup refresh layout
         refreshLayout = findViewById<SwipeRefreshLayout>(R.id.refreshLayout).apply {
             setOnRefreshListener {
+
+                /*
+                Tell the SDK to load the latest data from the API
+                 */
                 storytellerRowView.reloadData({
                     refreshLayout.isRefreshing = false
                 })
@@ -55,30 +79,58 @@ class MainActivity : AppCompatActivity(), StorytellerRowViewDelegate {
         handleDeepLink(intent?.data)
     }
 
+    /*
+    Called when the data loading network request is complete
+    For more info, see - https://docs.getstoryteller.com/documents/android-sdk/StorytellerRowViewDelegate#error-handling
+     */
     override fun onStoryDataLoadComplete(success: Boolean, error: Error?, dataCount: Int) {
         Log.i("Storyteller Sample", "onChannelsDataLoadComplete callback: success $success, error $error, dataCount $dataCount")
     }
 
+    /*
+    Called when the network request to load data for all stories has started
+     */
     override fun onStoryDataLoadStarted() {
         Log.i("Storyteller Sample", "onStoryDataLoadStarted callback")
     }
 
+    /*
+    Called when any story has been dismissed
+     */
     override fun onStoryDismissed() {
         Log.i("Storyteller Sample", "onStoryDismissed callback")
     }
 
+    /*
+     Called when an analytics event is triggered
+     For more info, see - https://docs.getstoryteller.com/documents/android-sdk/StorytellerRowViewDelegate#analytics
+     */
     override fun onUserActivityOccurred(type: UserActivity.EventType, data: UserActivityData) {
         Log.i("Storyteller Sample", "onUserActivityOccurred: type $type data $data")
     }
 
+    /*
+    Called whenever a tile is visible in the story view
+    For more info, see - https://docs.getstoryteller.com/documents/android-sdk/StorytellerRowViewDelegate#tile-visibility
+     */
     override fun tileBecameVisible(storyIndex: Int) {
         Log.i("Storyteller Sample", "tileBecameVisible: storyIndex $storyIndex")
     }
 
+    /*
+    Called when a user swipes up on a page which should direct the user
+    to a specific place within the integrating app.
+    For more info, see - https://docs.getstoryteller.com/documents/android-sdk/StorytellerRowViewDelegate#swiping-up-to-the-integrating-app
+     */
     override fun userSwipedUpToApp(swipeUpUrl: String) {
         Log.i("Storyteller Sample", "userSwipedUpToApp: swipeUpUrl $swipeUpUrl")
     }
 
+    /*
+    Called when the tenant is configured to request ads from the containing app
+    and the SDK requires ad data from the containing app
+    For more info, see - https://docs.getstoryteller.com/documents/android-sdk/StorytellerRowViewDelegate#client-ads
+     */
     override fun getAdsForRow(stories: List<ClientStory>, onComplete: (AdResponse) -> Unit) {
         Log.i("Storyteller Sample", "getAdsForRow: stories $stories")
     }
