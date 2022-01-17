b9eaf6df63bc19e9c16b5edbefc662458e6c62fa
michaldabrowskistorm <michal.dabrowski@stormideas.com>
2022-01-17 16:01:22 +0100

    Sample app adopted to sdk 6.0

--- app/src/main/java/com/example/storytellerSampleAndroid/OtherActivity.kt
+++ app/src/main/java/com/example/storytellerSampleAndroid/OtherActivity.kt
@@ -1,20 +1,8 @@
 package com.example.storytellerSampleAndroid
 
-import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.Toast
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import com.storyteller.Storyteller
-import com.storyteller.domain.*
-import com.storyteller.services.Error
-import com.storyteller.ui.row.StorytellerRowView
-import com.storyteller.ui.row.StorytellerRowViewDelegate
-import java.util.*
+import androidx.appcompat.app.AppCompatActivity
 
 class OtherActivity : AppCompatActivity(R.layout.activity_other) {
     override fun onCreate(savedInstanceState: Bundle?) {
