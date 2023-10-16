9f39a9158bf565bee06e821f30a186f3a3230810
AbdElraoufSabri <abdelraoufsabri@gmail.com>
2023-10-16 15:15:38 +0300

    9.7.2

--- app/src/main/java/com/example/storytellerSampleAndroid/EmbeddedClipActivity.kt
+++ app/src/main/java/com/example/storytellerSampleAndroid/EmbeddedClipActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.storyteller.ui.pager.StorytellerClipsFragment
 
-class EmbeddedClipActivity : AppCompatActivity(R.layout.activity_embeded_clip){
+class EmbeddedClipActivity : AppCompatActivity(R.layout.activity_embeded_clip) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val clipFragment = StorytellerClipsFragment.create("clipssample")
+        val clipFragment = StorytellerClipsFragment.create(
+            collectionId = "clipssample",
+            topLevelBackEnabled = true,
+        ).apply {
+            onTopLevelBackPressed = {
+                finish()
+                true
+            }
+        }
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.add(R.id.fragment_container_view,clipFragment)
+        transaction.add(R.id.fragment_container_view, clipFragment)
         transaction.commit()
     }
-
 }
