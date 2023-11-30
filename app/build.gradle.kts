58a5c9c2d855053c3912fe0af30d23de13f832f2
Filip Nabrdalik <filipnabrdalik@gmail.com>
2023-11-30 10:09:42 +0100

    Clean up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,6 @@ dependencies {
     implementation("androidx.compose.material:material")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation ("androidx.compose.ui:ui-viewbinding")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("androidx.compose.runtime:runtime:1.5.4")
