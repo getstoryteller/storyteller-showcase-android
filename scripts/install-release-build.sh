dfb118549c46f7553f38059a051bf9b993e1c837
AbdElraoufSabri <abdelraoufsabri@gmail.com>
2023-12-12 18:46:12 +0200

    execute clean task in the apk launching script

--- scripts/install-release-build.sh
+++ scripts/install-release-build.sh
@@ -1,2 +1,2 @@
-bash gradlew :app:assembleRelease installRelease
+bash gradlew clean :app:assembleRelease installRelease
 adb shell am start -n "com.getstoryteller.storytellersampleapp/com.getstoryteller.storytellersampleapp.features.main.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
