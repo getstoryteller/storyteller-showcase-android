0480cd2768e88ffb4113f44a5aa566de26a98571
AbdElraoufSabri <abdelraoufsabri@gmail.com>
2023-12-12 18:22:45 +0200

    remove animated visibility

--- app/src/main/java/com/getstoryteller/storytellersampleapp/features/main/bottomnav/BottomNavigationBar.kt
+++ app/src/main/java/com/getstoryteller/storytellersampleapp/features/main/bottomnav/BottomNavigationBar.kt
@@ -1,6 +1,5 @@
 package com.getstoryteller.storytellersampleapp.features.main.bottomnav
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
@@ -24,93 +23,91 @@ fun BottomNavigationBar(
   onSetNavigationState: (PageState) -> Unit,
   onTriggerMomentReload: () -> Unit,
   onSetNavigationInterceptor: () -> NavigationInterceptor = { NavigationInterceptor.None },
-  ) {
-  AnimatedVisibility(
-    visible = navigationState == PageState.HOME,
-    content = {
-      BottomNavigation(
-        backgroundColor = MaterialTheme.colors.background
-      ) {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val homeSelected = navBackStackEntry?.destination?.route == "home"
-        val coroutineScope = rememberCoroutineScope()
-        BottomNavigationItem(
-          icon = {
-            Icon(
-              painter = painterResource(id = R.drawable.ic_home),
-              contentDescription = null,
-              tint = if (homeSelected) MaterialTheme.colors.primary else MaterialTheme.colors.onSurface
-            )
-          },
-          label = {
-            Text(
-              text = "Home",
-              color = if (homeSelected) MaterialTheme.colors.primary else MaterialTheme.colors.onSurface
-            )
-          },
-          selected = navBackStackEntry?.destination?.route == "home",
-          onClick = {
-            val interceptor = onSetNavigationInterceptor()
-            if (interceptor is NavigationInterceptor.TargetRoute &&
-              homeSelected && interceptor.shouldIntercept()
-            ) {
-              coroutineScope.launch {
-                interceptor.onIntercepted()
-              }
-              return@BottomNavigationItem
+) {
+  if (navigationState == PageState.HOME) {
+    BottomNavigation(
+      backgroundColor = MaterialTheme.colors.background
+    ) {
+      val navBackStackEntry by navController.currentBackStackEntryAsState()
+      val homeSelected = navBackStackEntry?.destination?.route == "home"
+      val coroutineScope = rememberCoroutineScope()
+      BottomNavigationItem(
+        icon = {
+          Icon(
+            painter = painterResource(id = R.drawable.ic_home),
+            contentDescription = null,
+            tint = if (homeSelected) MaterialTheme.colors.primary else MaterialTheme.colors.onSurface
+          )
+        },
+        label = {
+          Text(
+            text = "Home",
+            color = if (homeSelected) MaterialTheme.colors.primary else MaterialTheme.colors.onSurface
+          )
+        },
+        selected = navBackStackEntry?.destination?.route == "home",
+        onClick = {
+          val interceptor = onSetNavigationInterceptor()
+          if (interceptor is NavigationInterceptor.TargetRoute &&
+            homeSelected && interceptor.shouldIntercept()
+          ) {
+            coroutineScope.launch {
+              interceptor.onIntercepted()
             }
-
-            navController.navigate("home") {
-              onSetNavigationState(PageState.HOME)
-              popUpTo(navController.graph.startDestinationId) {
-                saveState = true
-              }
-              launchSingleTop = true
-              restoreState = true
-            }
-            onSetTopBarVisible(true)
+            return@BottomNavigationItem
           }
-        )
-        BottomNavigationItem(
-          icon = {
-            Icon(
-              painter = painterResource(id = R.drawable.ic_watch),
-              contentDescription = null,
-              tint = if (!homeSelected) MaterialTheme.colors.primary else MaterialTheme.colors.onSurface
-            )
-          },
-          label = {
-            Text(
-              text = "Moments",
-              color = if (!homeSelected) MaterialTheme.colors.primary else MaterialTheme.colors.onSurface
-            )
-          },
-          selected = navBackStackEntry?.destination?.route == "home/moments",
-          onClick = {
-            val interceptor = onSetNavigationInterceptor()
-            if (interceptor is NavigationInterceptor.TargetRoute &&
-              !homeSelected && interceptor.shouldIntercept()
-            ) {
-              coroutineScope.launch {
-                interceptor.onIntercepted()
-              }
-              return@BottomNavigationItem
-            }
 
-            if (navBackStackEntry?.destination?.route == "home/moments") {
-              onTriggerMomentReload()
-            }
+          navController.navigate("home") {
             onSetNavigationState(PageState.HOME)
-            onSetTopBarVisible(false)
-            navController.navigate("home/moments") {
-              popUpTo(navController.graph.startDestinationId) {
-                saveState = true
-              }
-              launchSingleTop = true
-              restoreState = true
+            popUpTo(navController.graph.startDestinationId) {
+              saveState = true
             }
+            launchSingleTop = true
+            restoreState = true
           }
-        )
-      }
-    })
+          onSetTopBarVisible(true)
+        }
+      )
+      BottomNavigationItem(
+        icon = {
+          Icon(
+            painter = painterResource(id = R.drawable.ic_watch),
+            contentDescription = null,
+            tint = if (!homeSelected) MaterialTheme.colors.primary else MaterialTheme.colors.onSurface
+          )
+        },
+        label = {
+          Text(
+            text = "Moments",
+            color = if (!homeSelected) MaterialTheme.colors.primary else MaterialTheme.colors.onSurface
+          )
+        },
+        selected = navBackStackEntry?.destination?.route == "home/moments",
+        onClick = {
+          val interceptor = onSetNavigationInterceptor()
+          if (interceptor is NavigationInterceptor.TargetRoute &&
+            !homeSelected && interceptor.shouldIntercept()
+          ) {
+            coroutineScope.launch {
+              interceptor.onIntercepted()
+            }
+            return@BottomNavigationItem
+          }
+
+          if (navBackStackEntry?.destination?.route == "home/moments") {
+            onTriggerMomentReload()
+          }
+          onSetNavigationState(PageState.HOME)
+          onSetTopBarVisible(false)
+          navController.navigate("home/moments") {
+            popUpTo(navController.graph.startDestinationId) {
+              saveState = true
+            }
+            launchSingleTop = true
+            restoreState = true
+          }
+        }
+      )
+    }
+  }
 }
