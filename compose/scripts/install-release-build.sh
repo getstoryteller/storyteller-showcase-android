f4007ffd564805efba64c1ff882f5bf870d35445
abd3lraouf <abdelraoufsabri@gmail.com>
2024-03-11 13:58:28 +0200

    :bug: fix: Prevent overwriting of nightly build files
    This commit resolves an issue where nightly build zip files were being overwritten if they were generated on the same day. An incrementing counter has been added to ensure unique file names for each new build. Additionally, the order of assembling debug and release builds has been optimized in a single gradlew command for efficiency.

--- compose/scripts/install-release-build.sh
+++ compose/scripts/install-release-build.sh
@@ -1,11 +1,17 @@
 TODAYS_DATE=$(date +%Y%m%d)
+INCREMENT=0
 APK_RELEASE="Showcase-apk-nightly-release-$TODAYS_DATE.apk"
 APK_DEBUG="Showcase-apk-nightly-debug-$TODAYS_DATE.apk"
 ZIP_NAME="Showcase-apk-nightly-$TODAYS_DATE.zip"
 
+while [ -f "./$ZIP_NAME" ]
+do
+  INCREMENT=$((INCREMENT + 1))
+  ZIP_NAME="Showcase-apk-nightly-$TODAYS_DATE-$INCREMENT.zip"
+done
+
 if [ "$1" == "zip" ]; then
-    ./gradlew :app:assembleRelease
-    ./gradlew :app:assembleDebug
+    ./gradlew :app:assembleDebug :app:assembleRelease
     cp "./app/build/outputs/apk/release/app-release.apk" "./$APK_RELEASE"
     cp "./app/build/outputs/apk/debug/app-debug.apk" "./$APK_DEBUG"
     zip "./$ZIP_NAME" "./$APK_RELEASE" "./$APK_DEBUG"
