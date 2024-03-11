607d2dcc4c7c3d33f59dbcf16bb8d89d222aa720
abd3lraouf <abdelraoufsabri@gmail.com>
2024-03-11 11:09:39 +0200

    :lock: feat: add proxyman config in debug mode only

--- compose/scripts/install-release-build.sh
+++ compose/scripts/install-release-build.sh
@@ -1,12 +1,18 @@
-bash gradlew :app:assembleRelease installRelease
-adb shell am start -n "com.getstoryteller.storytellershowcaseapp/com.getstoryteller.storytellershowcaseapp.ui.features.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
-
 TODAYS_DATE=$(date +%Y%m%d)
+APK_RELEASE="Showcase-apk-nightly-release-$TODAYS_DATE.apk"
+APK_DEBUG="Showcase-apk-nightly-debug-$TODAYS_DATE.apk"
+ZIP_NAME="Showcase-apk-nightly-$TODAYS_DATE.zip"
+
 if [ "$1" == "zip" ]; then
-    ZIP_NAME="Showcase-apk-nightly-$TODAYS_DATE.zip"
-    APK_NAME="Showcase-apk-nightly-$TODAYS_DATE.apk"
-    cp "./app/build/outputs/apk/release/app-release.apk" "./$APK_NAME"
-    zip "./$ZIP_NAME" "./$APK_NAME"
-    rm "./$APK_NAME"
+    ./gradlew :app:assembleRelease
+    ./gradlew :app:assembleDebug
+    cp "./app/build/outputs/apk/release/app-release.apk" "./$APK_RELEASE"
+    cp "./app/build/outputs/apk/debug/app-debug.apk" "./$APK_DEBUG"
+    zip "./$ZIP_NAME" "./$APK_RELEASE" "./$APK_DEBUG"
+    rm "./$APK_RELEASE"
+    rm "./$APK_DEBUG"
     open .
+else
+    ./gradlew :app:assembleRelease installRelease
+    adb shell am start -n "com.getstoryteller.storytellershowcaseapp/com.getstoryteller.storytellershowcaseapp.ui.features.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
 fi
